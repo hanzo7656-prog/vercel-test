b6dabd104f11e214d39183ff5af3b1e157bf259c
hanzo7656-prog <hanzo7656@gmail.com>
2026-08-13 14:40:22 +0330

    Update and rename wsgi.py to model.json

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from mother_health import MotherHealth
-
-API_KEY = os.environ.get("API_KEY", "40QRC4gdyzWIGwsvGkqWtcDOf0bk+FV217KmLxQ/Wmw=")
-health = MotherHealth(api_key=API_KEY, port=0)
-app = health.app
