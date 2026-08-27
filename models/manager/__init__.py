6a9615a3a3715775edba6cd728c971a9b3d5e304
hanzo7656-prog <hanzo7656@gmail.com>
2026-08-27 16:55:02 +0330

    Create __init__.py

---
+++ models/manager/__init__.py
@@ -0,0 +1,10 @@
+# models/manager/__init__.py
+# ============================================================
+# پکیج مدیریت مدل
+# ============================================================
+
+from models.manager.model_manager import ModelManager
+
+__all__ = [
+    'ModelManager'
+]
