0ce9351c87a248c4ec07e074c348e01b0862d829
hanzo7656-prog <hanzo7656@gmail.com>
2026-08-30 07:55:12 +0330

    Create __init__.py

---
+++ infrastructure/external/__init__.py
@@ -0,0 +1,11 @@
+# infrastructure/external/__init__.py
+# ============================================================
+# External Services - سرویس‌های خارجی
+# ============================================================
+
+from infrastructure.external.alerter import alerter, Alerter
+
+__all__ = [
+    'alerter',
+    'Alerter'
+]
